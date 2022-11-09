1414b95bd0d13bae20e0b1683f285c424ad43f7f
kimym56 <74525390+kimym56@users.noreply.github.com>
2022-11-09 15:34:07 +0900

    1109

--- src/App.js
+++ src/App.js
@@ -26,19 +26,20 @@ import "./style.css"
 export default function App (){
 
   // const [rad, setRad] = useState(45.35);
-  let rad = 45.35; // 도형의 너비
+  let rad = (Math.random()*56.692913386 + 34.015748031); // 도형의 너비
   let xpos, ypos; // 도형의 시작점
   let xspeed = 9; // 도형의 속도
   let yspeed = 12; // 도형의 속도
+  let speed = (Math.random()*192.37795276/60)
   
-  let xdirection = 1; // 왼쪽 또는 오른쪽
-  let ydirection = -1; // 위 또는 아래
+  let xdirection = Math.random()*2-1; // 왼쪽 또는 오른쪽
+  let ydirection = Math.random()*2-1; // 위 또는 아래
   let success = false;
   let pressed = 0;
   let writer;
   const [mouse , setMouse] = useState();
 	const setup = (p5, parentRef) => {
-		p5.createCanvas(p5.windowWidth/2, p5.windowHeight/2).parent(parentRef);
+		p5.createCanvas(p5.windowWidth*4/5, p5.windowHeight*4/5).parent(parentRef);
     p5.frameRate(60);
     p5.ellipseMode(p5.RADIUS);
     xpos = p5.random(1+rad,p5.width-rad);
@@ -50,9 +51,9 @@ export default function App (){
 		p5.background(0);
     success=false;
 		// 도형의 위치 업데이트
-    xpos = xpos + xspeed * xdirection;
-    ypos = ypos + yspeed * ydirection;
-  
+    xpos = xpos + (speed * xdirection/Math.sqrt(Math.pow(xdirection)+Math.pow(ydirection)));
+    ypos = ypos + (speed * ydirection/Math.sqrt(Math.pow(xdirection)+Math.pow(ydirection)));
+    console.log('here',xdirection,ydirection,speed);
     // 도형이 화면 경계를 넘어가는 지 테스트
     // 넘어갈 경우, -1을 곱하여 방향을 반대로 돌린다.
     if (xpos > p5.width - rad || xpos < rad) {
@@ -87,11 +88,19 @@ export default function App (){
   const mousePressed=(p5)=>{
     if (p5.overBox) {
       success = true;
+      rad = (Math.random()*56.692913386 + 34.015748031)
+      xdirection = Math.random()*2-1; // 왼쪽 또는 오른쪽
+      ydirection = Math.random()*2-1; // 위 또는 아래
+      speed = (Math.random()*192.37795276/60)
       p5.fill(255, 255, 0);
       xpos = p5.random(1+rad, p5.width-rad);
       ypos = p5.random(1+rad, p5.height-rad);
     } else {
       success = false;
+      rad = (Math.random()*56.692913386 + 34.015748031)
+      xdirection = Math.random()*2-1; // 왼쪽 또는 오른쪽
+      ydirection = Math.random()*2-1; // 위 또는 아래
+      speed = (Math.random()*192.37795276/60)
       xpos = p5.random(1+rad, p5.width-rad);
       ypos = p5.random(1+rad, p5.height-rad);
     }
